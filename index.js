bd47fd4893d5e01d8d3bd543c73664b3072cf17f
ankitx23 <ankitxydv@gmail.com>
2025-03-06 23:08:27 -0500

    asynchronus

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8'); // enter the path of the file to print the content of it
 
 console.log(textIn);
-
+//blocking , Synchronus way
 //read and  write file in node.js
 const textOut = ' This is what we know about the avocado: ' + textIn + '\nCreated on ' + Date.now();
 fs.writeFileSync('./final/txt/output.txt', textOut);
@@ -13,4 +13,10 @@ console.log('File written!');
 //Also, synchronus is single thread // also blocking way
 
 //Asynchronus- time consuming tasks are executed in background in the mean time rest process can be executed in synchronus 
-//it is called non blocking method and it uses callback methods (we use more it in node js)
+//it is called non blocking method and it uses callback methods (we use more it in node js) //but it doesnt make them asynchornus code
+
+
+//Non blocking, synchronus way
+fs.readFile('./final/txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
