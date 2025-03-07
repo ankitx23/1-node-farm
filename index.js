ee43a7660df1bb3589d80de3b330362566c754ed
ankitx23 <ankitxydv@gmail.com>
2025-03-06 22:59:44 -0500

    Node.js and NPM

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8'); // enter the path of the file to print the content of it
+
+console.log(textIn);
+
+//read and  write file in node.js
+const textOut = ' This is what we know about the avocado: ' + textIn + '\nCreated on ' + Date.now();
+fs.writeFileSync('./final/txt/output.txt', textOut);
+console.log('File written!');
+
+//Synchronus- processes aare executed one by one if first process is taking then the rest have to wait for the completion of the first one.
+//Also, synchronus is single thread // also blocking way
+
+//Asynchronus- time consuming tasks are executed in background in the mean time rest process can be executed in synchronus 
+//it is called non blocking method and it uses callback methods (we use more it in node js)
